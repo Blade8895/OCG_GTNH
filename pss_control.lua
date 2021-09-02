b420ac2a23911de90958f0e9ab21a2612a464150
Blade8895 <76847661+Blade8895@users.noreply.github.com>
2021-09-02 16:01:24 +0200

    Update pss_control.lua
    + UI Cleanup
    + Automatic Function
    + configs
    + speedup

--- pss_control.lua
+++ pss_control.lua
@@ -4,33 +4,51 @@ aw = require("adWrite")
 sides = require("sides")
 event = require("event")
 
+-- Configs
+local version = "1.5"
+local auto = 1
+local rs_output = sides.top
+local Voltage = "ZPM "
+local on = 35
+local off = 95
+
 -- initial setup
  component.gpu.setResolution(43,12)
  term.clear()
  term.setCursor(1,1)
 -- end
 
--- Power-Sub Station Prozentrechner
+-- Power-Sub Station calc & percent converter
 function pss_calc()
  pss_max = component.gt_machine.getEUCapacity()
  pss_actual = component.gt_machine.getStoredEU()
  pss_percent = pss_actual/pss_max*100
  pss_print = string.format("%.2f", pss_percent)
 end
--- end  
+-- end 
+
+-- Automatik Turn on / off Function
+function auto_rs()
+ if pss_percent >= off then
+  component.redstone.setOutput(rs_output, 0)
+ end
+ if pss_percent <= on then
+  component.redstone.setOutput(rs_output, 15)
+ end
+end
  
 -- UI Core
 function UI_Build()
  print("|=========================================|")
- print("| PSS Control     1.0 | made by Blade8895 |")      
+ print("| PSS Control     "..version.." | made by Blade8895 |")      
  print("|=========================================|")
- print("|  PSS % =                                |")
- print("|  EU/t IN  =                             |")
- print("|  EU/t Out =                             |")
+ print("|                                         |")
+ print("|                                         |")
+ print("| PSS % =                                 |")
  print("|                                         |")
  print("|                                         |")
  print("|=========================================|")
- print("| Charge State:          Voltage=         |")
+ print("| Charge State:              Voltage= "..Voltage.."|")
  print("|=========================================|")
 end
 -- UI Core end
@@ -38,27 +56,26 @@ end
 -- Beschriftung
 function refresh()
  pss_calc()
- os.sleep(0.5)  
+ if auto == 1 then 
+  auto_rs()
+ end
+ os.sleep(0.1)
 
- term.setCursor(12,4)
+ term.setCursor(12,6)
  aw.write("<color fg=0xffff00>"..pss_print.."% </color>")
 
- --if component.redstone.getOutput(sides.east) == 15 then
-  --term.setCursor(16,5)
-  --aw.write("<color bg=0x00ff00>ON</color>/OFF")
-  --term.setCursor(16,6)
-  --aw.write("<color bg=0x00ff00>ON</color>/OFF")
- --else
-  --term.setCursor(16,5)
-  --aw.write("ON/<color bg=0xff0000>OFF</color>")
-  --term.setCursor(16,6)
-  --aw.write("ON/<color bg=0xff0000>OFF</color>")
- --end
+ if component.redstone.getOutput(rs_output) == 15 then
+  term.setCursor(16,10)
+  aw.write("<color bg=0x00ff00>Charging</color>   ")
+ else
+  term.setCursor(16,10)
+  aw.write("<color bg=0xff0000>Discharging</color>   ")
+ end
 end
 
 UI_Build()
 
 while true do
  refresh()
- os.sleep(0.5)
+ os.sleep(0.1)
 end
